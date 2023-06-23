e77776db659386b50f735c97250041d99daaead5
Ann <55690276+annafanaseva@users.noreply.github.com>
2023-06-23 17:37:11 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './normalize.css';
 import './index.css';
 import App from './App';
@@ -8,10 +8,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 reportWebVitals();
